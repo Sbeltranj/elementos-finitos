2fbdcb8aa6c08f2f3330d8d2a45b48750d8adfb1
Sbeltranj <sbeltran@unal.edu.co>
2022-02-12 14:02:05 -0500

    Spline

--- 1D_EF/EF_barra_n_nodos/Algoritmos_interpola.jl
+++ 1D_EF/EF_barra_n_nodos/Algoritmos_interpola.jl
@@ -1,4 +1,5 @@
 using Interpolations, PyPlot, PyCall, MappedArrays
+using CubicSplines
 
 #https://juliamath.github.io/Interpolations.jl/latest/api/
 
@@ -8,18 +9,26 @@ pygui(true)
 ## Se crea un espacio para hacer clic y definir los nodos del EF
 figure()
 title("Haciendo clic con el ratón, defina los 10 puntos a interpolar")
-axis([-5, 5, -5, 5])
+axis([-6, 6, -6, 6])
 
 coord = ginput(10)
-xi = collect(1:1:10)
+xi   = collect(1:1:10)
 xnod = mappedarray(coord->first(coord),coord)
 ynod = mappedarray(coord->last(coord),coord)
 
 
 itp = interpolate(ynod, BSpline(Linear()))
+#itp1 = interpolate(ynod, BSpline(NoInterp()))
+#itp = CubicSplineInterpolation(ynod, xi)
 
+spline = CubicSpline(xnod, ynod)
+xs = range(xnod[1], stop=xnod[end], length=1000)
+ys = spline[xs]
 
+plt.grid()
+plot(xnod, ynod, "o")
+plot(xnod, itp)
+plot(xs, ys)
+#plot(xnod, itp1)
 
-
-
-
+##SIN TERMINAR-FALTA FALTA
--- Install_Pkg.jl
+++ Install_Pkg.jl
@@ -4,7 +4,7 @@
 import Pkg
 
 packages = ["Plots", "Polynomials", "PyPlot", "LinearAlgebra", "Statistics", "SparseArrays", "PyCall",
-             "XLSX", "BoundaryValueDiffEq", "PlotlyJS", "MappedArrays", "WriteVTK" ]
+             "XLSX", "BoundaryValueDiffEq", "PlotlyJS", "MappedArrays", "WriteVTK", "Interpolations", "CubicSplines" ]
 
 for e = 1:length(packages)
  Pkg.add(packages[e])
